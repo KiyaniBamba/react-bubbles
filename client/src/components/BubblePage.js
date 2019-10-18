b28a0dfd70c3937b5f41dbc0280af690b3ce8550
kiyani <bamba.kiyani@gmail.com>
2019-10-18 04:08:14 -0400

    built login form to authenticate users

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -4,11 +4,14 @@ import axios from "axios";
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
+
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
 
+  
+  
   return (
     <>
       <ColorList colors={colorList} updateColors={setColorList} />
