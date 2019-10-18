9e1242e36a5dc01beecfc258017d99b793dc0a10
kiyani <bamba.kiyani@gmail.com>
2019-10-18 04:49:00 -0400

    fetched colors data from the servers in BubblePage component

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -10,14 +10,37 @@ const BubblePage = () => {
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
 
-  
-  
-  return (
-    <>
-      <ColorList colors={colorList} updateColors={setColorList} />
-      <Bubbles colors={colorList} />
-    </>
-  );
+  useEffect(() => {
+    const colorsData =() => {
+      axiosWithAuth()
+      .get('http://localhost:5000/api/colors')
+      .then(response => {
+				setColorList(response.data);
+			})
+      .catch(error =>{
+        console.log(error);
+      })
+    }
+    colorsData();
+  }, []);
+	return (
+		<>
+			<ColorList colors={colorList} updateColors={setColorList} />
+			<Bubbles colors={colorList} />
+		</>
+	);
 };
 
 export default BubblePage;
+
+const axiosWithAuth = () => {
+  const token=localStorage.getItem('token');
+  console.log( token);
+
+  return axios.create({
+    headers: {
+      'Content-Type': 'apllication/json',
+      'Authorization': '${token}',
+    },
+  });
+};
